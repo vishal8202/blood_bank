93c2613c4e7b02c2e7ff3543f0da4d59a84ef5ba
vishal8202 <vishalvijayan222@gmail.com>
2022-11-06 11:02:08 +0530

    first commit

---
+++ blood.py
@@ -0,0 +1,22 @@
+while True:
+    print("Select  : ")
+    print("""        1. Add details
+            2. view details
+            3. search details
+            4. update details
+            5. delete details
+            6. exit
+             """)
+    choice = int(input("Enter your choice : "))
+    if choice==1:
+        print("selected add details")
+        name = input("Enter the name : ")
+        bloodgroup = input("Enter the blood group : ")
+        unit = input("Enter the unit : ")
+        phone = input("Enter phone number : ")
+        place = input("Enter place : ")
+        
+
+
+
+    break
